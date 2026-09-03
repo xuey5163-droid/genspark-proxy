eeff78963e4380f1746141e4b9e1ff573ed9644c
xuey5163-droid <xuey5163@gmail.com>
2026-09-03 14:45:59 +0800

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,45 +1,23 @@
-import { execFile } from "child_process";
-
 export default async function handler(req, res) {
   try {
-    const result = await new Promise((resolve, reject) => {
-      execFile(
-        process.execPath,
-        [
-          "/var/task/node_modules/@genspark/cli/dist/index.js",
-          "--version"
-        ],
-        {
-          env: process.env,
-          timeout: 10000
-        },
-        (error, stdout, stderr) => {
-          if (error) {
-            reject({
-              message: error.message,
-              code: error.code,
-              stdout,
-              stderr
-            });
-          } else {
-            resolve({
-              stdout,
-              stderr
-            });
-          }
-        }
-      );
-    });
+    const genspark = await import("@genspark/cli");
 
     return res.status(200).json({
       success: true,
-      result
+      module_type: typeof genspark,
+      exports: Object.keys(genspark),
+      collectDesignAttachmentArgs: typeof genspark.collectDesignAttachmentArgs,
+      function_source:
+        typeof genspark.collectDesignAttachmentArgs === "function"
+          ? genspark.collectDesignAttachmentArgs.toString().substring(0, 3000)
+          : null
     });
 
   } catch (error) {
     return res.status(500).json({
       success: false,
-      error
+      error: error.message,
+      stack: error.stack
     });
   }
 }
