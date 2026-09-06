78797b03b5d1d2eee5365b751289ee386542558c
xuey5163-droid <xuey5163@gmail.com>
2026-09-06 20:43:29 +0800

    Update test.js

--- api/test.js
+++ api/test.js
@@ -5,15 +5,13 @@ const cli = "/var/task/node_modules/@genspark/cli/dist/index.js";
 export default function handler(req, res) {
   execFile(
     process.execPath,
-    [cli, "--version"],
+    [cli, "task", "--help"],
     {
       timeout: 30000
     },
     (error, stdout, stderr) => {
       return res.status(200).json({
         success: !error,
-        node: process.execPath,
-        cli,
         error: error ? error.message : null,
         code: error ? error.code : null,
         stdout: stdout || "",
