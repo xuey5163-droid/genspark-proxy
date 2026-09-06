266367775b664916fa08af96ba2768837849f019
xuey5163-droid <xuey5163@gmail.com>
2026-09-06 20:34:46 +0800

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,28 +1,24 @@
-import { readFileSync } from "node:fs";
+import { execFile } from "node:child_process";
+
+const cli = "/var/task/node_modules/@genspark/cli/dist/index.js";
 
 export default function handler(req, res) {
-  try {
-    const packageJson = JSON.parse(
-      readFileSync(
-        "/var/task/node_modules/@genspark/cli/package.json",
-        "utf8"
-      )
-    );
-
-    return res.status(200).json({
-      success: true,
-      name: packageJson.name,
-      version: packageJson.version,
-      type: packageJson.type,
-      main: packageJson.main,
-      bin: packageJson.bin,
-      exports: packageJson.exports
-    });
-
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      error: error.message
-    });
-  }
+  execFile(
+    process.execPath,
+    [cli, "--version"],
+    {
+      timeout: 30000
+    },
+    (error, stdout, stderr) => {
+      return res.status(200).json({
+        success: !error,
+        node: process.execPath,
+        cli,
+        error: error ? error.message : null,
+        code: error ? error.code : null,
+        stdout: stdout || "",
+        stderr: stderr || ""
+      });
+    }
+  );
 }
